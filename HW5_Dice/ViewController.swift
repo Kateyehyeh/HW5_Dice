3a63d2de9c6b6758cae36297f76dbad4046d0b2e
Kate Yeh <kateyehyeh@gmail.com>
2025-01-12 20:06:39 +0800

    Initial Commit

---
+++ HW5_Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5_Dice
+//
+//  Created by Kateyeh on 2025/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
