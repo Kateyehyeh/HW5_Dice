17ae6074e7b3ff7bde71023ea5d5b605d9a83d75
Kate Yeh <kateyehyeh@gmail.com>
2025-04-20 16:57:40 +0800

    Add all updated Swift files and project changes

--- HW5_Dice/ViewController.swift
+++ HW5_Dice/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    @IBOutlet weak var sumLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func playDice(_ sender: Any) {
+        let number1 = Int.random(in: 1...6)
+        let number2 = Int.random(in: 1...6)
+        let sum = number1 + number2
+        sumLabel.text = "\(sum)"
+        diceImage1.image = UIImage(systemName: "die.face.\(number1).fill")
+        diceImage2.image = UIImage(systemName: "die.face.\(number2).fill")
+    }
+    
+    
 }
 
+#Preview{
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    return storyboard.instantiateInitialViewController()!
+}
